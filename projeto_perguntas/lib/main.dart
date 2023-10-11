8cfbe770233e5aa7f5b30a63c1f449f4b1a0fb2e
Rodrigo Oliveira <rodrigo.oliveira@compasso.com.br>
2023-10-11 14:28:59 -0300

    Correções de nomenclatura segundo convenções do DART

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:projeto_perguntas/widgets/app/perguntaApp.dart';
+import 'package:projeto_perguntas/widgets/app/pergunta_app.dart';
 
 void main() => runApp(const PerguntaApp());
